c7d0fe0df41aa8e9442d4fec71a8135720791bc1
z <z@illusor.net>
2014-12-29 15:29:03 +0300

    top versions

--- py/calendar.cmd
+++ py/calendar.cmd
@@ -1,2 +1,4 @@
 src\\calendar.py ..\\result\\test-calendar-en-bottom.svg bottom en
 src\\calendar.py ..\\result\\test-calendar-ru-bottom.svg bottom ru
+src\\calendar.py ..\\result\\test-calendar-en-top.svg top en
+src\\calendar.py ..\\result\\test-calendar-ru-top.svg top ru
